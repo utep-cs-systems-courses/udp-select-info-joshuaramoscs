65dafd40790ecd4b08afb61ab9744285ab5e8171
student <student@systems-vm.localdomain>
2021-11-02 00:39:28 -0600

    Client File Transfer sends

--- FileTransferServer/client-fileTransfer.py
+++ FileTransferServer/client-fileTransfer.py
@@ -1,18 +1,13 @@
 #! /usr//bin/env python3
-# udp demo client.  Modified from Kurose/Ross by Eric Freudenthal 2016
+# File transfer client.  Modified from Kurose/Ross by Eric Freudenthal 2016
 
 from socket import *
-
+import sys, re
 
 # default params
 serverAddr = ('localhost', 50000)
 
-import sys, re
-
-def usage():
-    print("usage: %s [--serverAddr host:port]"  % sys.argv[0])
-    sys.exit(1)
-
+# server select code, could probably delete
 try:
     args = sys.argv[1:]
     while args:
@@ -24,18 +19,29 @@ try:
             print("unexpected parameter %s" % args[0])
             usage();
 except:
-    usage()
+    print("usage: %s [--serverAddr host:port]" % sys.argv[0])
+    sys.exit(1)
 
 
+# File transfer protocol
 print("serverAddr = %s" % repr(serverAddr))
-
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-print("Input file name")
-filename = sys.stdin.readline()[:-1]     # delete final \n
-f = open(filename, "r")
-fileContent = f.read()
-f.close()
 
-clientSocket.sendto(fileContent.encode(), serverAddr)
-confirmation, serverAddrPort = clientSocket.recvfrom(2048)
-print('Message from %s is "%s"' % (repr(serverAddrPort), confirmation.decode()))
+print("Input file name with extension:")
+filename = sys.stdin.readline()[:-1] # delete final \n
+
+try:
+    with open(filename, "rb") as f:  # "rb" ==  read in byte mode
+        # Send filename
+        clientSocket.sendto(filename.encode(), serverAddr)
+        print('Message from %s is "%s"' % (repr(serverAddrPort), confirmation.decode())
+        # Send file content
+        byte = f.read(2048)          # read 2048 bytes at a time
+        while byte:                  # while byte is not empty, send to server
+            clientSocket.sendto(byte, serverAddr)
+            confirmation, serverAddrPort = clientSocket.recvfrom(2048)
+            print('Message from %s is "%s"' % (repr(serverAddrPort), confirmation.decode())
+            byte = f.read(2048)
+            
+except IOError:
+    print('Could not read file %s' % filename)
